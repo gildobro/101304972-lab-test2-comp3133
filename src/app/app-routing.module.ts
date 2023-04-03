0dc60760935b01d105092e47381e7a689fc83de9
Gil Dobrovinsky <gdobrovinsky@gmail.com>
2023-04-03 17:22:40 -0400

    Query Updates, need to add navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { MissionListComponent } from './mission-list/mission-list.component';
 import { MissiondetailsComponent } from './missiondetails/missiondetails.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/missions', pathMatch: 'full'},
+  { path: '', redirectTo: 'missions', pathMatch: 'full' },
   { path: 'missions', component: MissionListComponent },
-  { path: 'missions/:flight_number', component: MissiondetailsComponent}
-]
-
-
+  { path: 'mission/:flight_number', component: MissiondetailsComponent },
+  { path: '**', redirectTo: 'missions' }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    [RouterModule.forRoot(routes)],
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

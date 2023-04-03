559a2eb93cde179771547929c6daee265ecfe025
Gil Dobrovinsky <gdobrovinsky@gmail.com>
2023-04-03 03:17:09 -0400

    Mission Search Filter + Updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MissionListComponent } from './mission-list/mission-list.component';
+import { MissionFilterComponent } from './mission-filter/mission-filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MissionListComponent,
+    MissionFilterComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/mission-list/mission-list.component.ts
+++ src/app/mission-list/mission-list.component.ts
@@ -8,13 +8,36 @@ import { HttpClient } from '@angular/common/http';
 })
 export class MissionListComponent implements OnInit {
   launches: any[] = [];
+  selectedYear: string = '';
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-      this.http.get<any[]>('https://api.spacexdata.com/v3/launches').subscribe(data => {
-        this.launches = data;
-      });
+      // this.http.get<any[]>('https://api.spacexdata.com/v3/launches').subscribe(data => {
+      //   this.launches = data;
+      // });
+      this.getLaunches();
   }
+
+  getLaunches() {
+    let apiUrl = 'https://api.spacexdata.com/v3/launches';
+    if (this.selectedYear) {
+      apiUrl += `?launch_year=${this.selectedYear}`;
+    }
+    this.http.get<any[]>(apiUrl).subscribe(
+      data => {
+        this.launches = data;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  OnYearSelected(year: string){
+    this.selectedYear = year;
+    this.getLaunches();
+  }
+
 
 }
